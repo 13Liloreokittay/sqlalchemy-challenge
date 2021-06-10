cd7b1852700a226a37b8531fe9a4ba2be642fb5b
Marissa Gallegos <78662343+13Liloreokittay@users.noreply.github.com>
2021-06-09 22:49:17 -0500

    final commit

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Station = Base.classes.station
+
+app = Flask(Hawaii.__init__)
